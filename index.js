d55f9a45a3ead9225c072168c8685628c82a9ab5
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2025-03-17 14:28:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+const connectDB = require("./connectMongo");
+
+connectDB();
+
+const BookModel = require("./models/book.model");
+
+app.get("/api/v1/books", async (req, res) => {
+  return res.status(200).json("response");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
